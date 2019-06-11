960540be783a8d63cc423e98b44582a779e4b031
Nikolai Bulashev <Nikolai_Bulashev@epam.com>
2019-06-11 21:56:13 +0300

    hw03

--- HW_03/RUNME.ps1
+++ HW_03/RUNME.ps1
@@ -1,7 +1,7 @@
 $RGName = 'hw03'
 $location = 'westeurope'
 $SAName = 'hw03startupfolder'
-$SAShareName = $RGName
+$blobContainerName = $RGName
 Get-AzResourceGroup -Name $RGName -ErrorVariable notPresent -ErrorAction SilentlyContinue
 
 if ($notPresent) {
@@ -10,21 +10,22 @@ if ($notPresent) {
 
 $storageAcct = Get-AzStorageAccount -ResourceGroupName $RGName -Name $SAName -ErrorVariable notPresentBucket -ErrorAction SilentlyContinue
 if ($notPresentBucket) {
+    Write-Host 'notPresent'
     $storageAcct = New-AzStorageAccount -ResourceGroupName $RGName -Name $SAName -Location $location -SkuName Standard_RAGRS -Kind StorageV2
-    $share = New-AzStorageShare -Name $SAShareName -Context $storageAcct.Context
+    new-AzStoragecontainer -Name $blobContainerName -Context $storageAcct.Context  -Permission blob
 } else {
+    Write-Host 'Present'
     $storageAcct = Get-AzStorageAccount -ResourceGroupName $RGName -Name $SAName -ErrorVariable notPresent -ErrorAction SilentlyContinue
-    $share = Get-AzStorageShare -Name $SAShareName -Context $storageAcct.Context
+    Get-AzStoragecontainer -Name $blobContainerName -Context $storageAcct.Context
 }
+#$token = New-AzureStorageAccountSASToken -ResourceType Service,Container,Object -Service file -Permission r -ExpiryTime (Get-Date).AddMinutes(30.0) -context $storageAcct.Context
 
-$token = New-AzStorageAccountSASToken -ResourceType Service,Container,Object -Service Blob,File,Table,Queue -Permission "ral" -context $storageAcct.Context
-
-#$token = New-AzStorageContainerSASToken -Name "Test" -Permission rwdl
 $localFileDirectory = '.\'
 $files = Get-ChildItem -Path $localFileDirectory -File
 foreach($file in $files)
 {
     $localFile = $localFileDirectory+$file
-    Set-AzStorageFileContent  -Share $share -Source $localFile -Force
+    set-AzStorageblobcontent  -File $localFile -Force -Container $blobContainerName -blob $file -Context $storageAcct.Context 
 }
-New-AzResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile 'Main.json' -containerSasToken $token -SAName $SAName -RGName $RGName 
+
+New-AzResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile 'Main.json' -SAName $SAName -RGName $RGName
