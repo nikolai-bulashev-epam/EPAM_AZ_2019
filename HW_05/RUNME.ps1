5a0e730c23b70e989a17ea4e7d58ca0c2c34369f
Nikolai Bulashev <Nikolai_Bulashev@epam.com>
2019-06-21 19:34:21 +0300

    HW05

---
+++ HW_05/RUNME.ps1
@@ -0,0 +1,29 @@
+$RGName = 'hw05'
+$location = 'westeurope'
+$SAName = 'hw04startupfolder'
+$blobContainerName = $RGName
+Get-AzResourceGroup -Name $RGName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+
+if ($notPresent) {
+    New-AzResourceGroup -Name $RGName -Location $location
+}
+
+$storageAcct = Get-AzStorageAccount -ResourceGroupName $RGName -Name $SAName -ErrorVariable notPresentBucket -ErrorAction SilentlyContinue
+if ($notPresentBucket) {
+    Write-Host 'notPresent'
+    $storageAcct = New-AzStorageAccount -ResourceGroupName $RGName -Name $SAName -Location $location -SkuName Standard_RAGRS -Kind StorageV2
+    new-AzStoragecontainer -Name $blobContainerName -Context $storageAcct.Context
+} else {
+    Write-Host 'Present'
+    $storageAcct = Get-AzStorageAccount -ResourceGroupName $RGName -Name $SAName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+    Get-AzStoragecontainer -Name $blobContainerName -Context $storageAcct.Context
+}
+
+$localFileDirectory = '.\'
+$files = Get-ChildItem -Path $localFileDirectory -File -Recurse
+foreach($file in $files)
+{
+    Write-Host $file
+    set-AzStorageblobcontent  -File $file.FullName -Force -Container $blobContainerName -blob $file -Context $storageAcct.Context 
+}
+New-AzResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile 'Main.json' -uniqueDnsNameForWebApp "hw05yetanotherwapp" -uniqueDnsName "hw05uniqueapp"
