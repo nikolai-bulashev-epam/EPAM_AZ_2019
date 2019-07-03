7903bb90cc97e77fcc27a6b1a101adf39a43ee11
Nikolai Bulashev <nikolai-bulashev-epam@gmail.com>
2019-07-03 12:02:34 +0300

    hw06

--- HW_06/RUNME.ps1
+++ HW_06/RUNME.ps1
@@ -2,6 +2,10 @@ $RGName = 'hw06'
 $location = 'westeurope'
 $SAName = $RGName+'startupfolder'
 $blobContainerName = $RGName
+$dbsuffix = -join ((48..57) + (97..122) | Get-Random -Count 5 | % {[char]$_})
+$dbstart = 'AdventureWorksLT'
+$dbmask = new-object System.String("AdventureWorksLT*")
+$dbname = $dbstart+$dbsuffix;
 
 function Get-RandomCharacters($length, $characters) {
     $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length }
@@ -15,19 +19,21 @@ function Scramble-String([string]$inputString){
     $outputString = -join $scrambledStringArray
     return $outputString 
 }
-
-
-$password = Get-RandomCharacters -length 5 -characters 'abcdefghiklmnoprstuvwxyz'
-$password += Get-RandomCharacters -length 1 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
-$password += Get-RandomCharacters -length 1 -characters '1234567890'
-$password += Get-RandomCharacters -length 1 -characters '!"ยง$%&/()=?}][{@#*+'
- 
- 
-$password = Scramble-String $password
-Write-Host "Your password is: $password" -ForegroundColor Green
-
-$SQLsecureStringPswd = ConvertTo-SecureString $password -AsPlainText -Force
-
+$vname = $RGname+"vault"
+$vault = Get-AzKeyVault -VaultName $vname -ErrorVariable notPresentVault -ErrorAction SilentlyContinue
+if (-not $vault) {
+    $password = Get-RandomCharacters -length 5 -characters 'abcdefghiklmnoprstuvwxyz'
+    $password += Get-RandomCharacters -length 5 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
+    $password += Get-RandomCharacters -length 2 -characters '1234567890'
+    $password += Get-RandomCharacters -length 2 -characters '!=@#'
+    $password = Scramble-String $password
+    Write-Host "Your password is: $password" -ForegroundColor Green
+    $SQLsecureStringPswd = ConvertTo-SecureString $password -AsPlainText -Force
+} else {
+    $password = (Get-AzKeyVaultSecret -vaultName "hw06vault" -name "sql01password").SecretValueText
+    Write-Host "You can use old password: $password" -ForegroundColor Green
+    $SQLsecureStringPswd = ConvertTo-SecureString $password -AsPlainText -Force 
+}
 Get-AzResourceGroup -Name $RGName -ErrorVariable notPresent -ErrorAction SilentlyContinue
 
 if ($notPresent) {
@@ -35,7 +41,7 @@ if ($notPresent) {
 }
 
 $storageAcct = Get-AzStorageAccount -ResourceGroupName $RGName -Name $SAName -ErrorVariable notPresentBucket -ErrorAction SilentlyContinue
-if ($notPresentBucket) {
+if (-Not $storageAcct) {
     Write-Host 'notPresent'
     $storageAcct = New-AzStorageAccount -ResourceGroupName $RGName -Name $SAName -Location $location -SkuName Standard_RAGRS -Kind StorageV2
     new-AzStoragecontainer -Name $blobContainerName -Context $storageAcct.Context
@@ -54,5 +60,28 @@ foreach($file in $files)
     Write-Host $file
     set-AzStorageblobcontent  -File $file.FullName -Force -Container $blobContainerName -blob $file -Context $storageAcct.Context 
 }
-New-AzResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile 'Main.json' -SASToken $token -RGName $RGName -SAName $SAName -TemplateParameterFile 'parameters.json' -sqlpassword $SQLsecureStringPswd 
+$fc = $files.count
+Write-Host "Startup folder is charged by $fc files" -ForegroundColor Green
 
+$azdb = Get-AzSqlDatabase -ServerName "sqlserver$RGName" -ResourceGroupName $RGName -DatabaseName "AdventureWorks*" -ErrorVariable nodbIsPresent -ErrorAction SilentlyContinue
+if ($azdb) {
+   Remove-AzSqlDatabase -DatabaseName $azdb.DatabaseName -ResourceGroupName $azdb.ResourceGroupName -ServerName $azdb.ServerName
+   Write-Host "Database will be deleted for redeploy" -ForegroundColor Yellow
+}
+New-AzResourceGroupDeployment -ResourceGroupName $RGName -TemplateFile 'Main.json' -SASToken $token -RGName $RGName -SAName $SAName -TemplateParameterFile 'parameters.json' -sqlpassword $SQLsecureStringPswd -databaseName $dbname
+$lastDeployment = Get-AzResourceGroupDeployment -ResourceGroupName $RGName | Sort Timestamp -Descending | Select -First 1
+if ($lastDeployment.ProvisioningState -eq 'Succeeded') {
+    Write-Host "-----------------------------------" -ForegroundColor Green
+    Write-Host 'Deployment was succeeded!' -ForegroundColor Green
+    $appURL = $lastDeployment.Outputs['trafficManagerFqdn'].Value 
+    Write-Host "Please visit $appURL to check application functionallity" -ForegroundColor Green
+    $ssdeURL = $lastDeployment.Outputs['sqlServerFqdn'].Value
+    Write-Host "sql server fqdn is $ssdeURL" -ForegroundColor Green
+    Write-Host "sql server login is sqladmin" -ForegroundColor Green
+    Write-Host "sql server password is $password" -ForegroundColor Green
+    Write-Host "-----------------------------------"
+} else {
+    Write-Host "-----------------------------------" -ForegroundColor Yellow
+    Write-Host 'Deployment was UNsucceeded!' -ForegroundColor Yellow
+    Write-Host "-----------------------------------"
+}
